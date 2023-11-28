fa4b32bebcf41b117e1df2165061b2f50892a7fe
simrett <leesim242@gmail.com>
2023-11-28 09:29:11 -0800

    modified

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# puppet declarative script to grab puppet-lint
-package { 'flask':
+# Install Python3 and pip3
+package { 'python3':
+  ensure => present,
+}
+
+package { 'python3-pip':
+  ensure => present,
+  require => Package['python3'],
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
+
