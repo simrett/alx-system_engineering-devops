2405fa2d870ea914a276d7b2e34529ee77b3c8d7
simrett <leesim242@gmail.com>
2023-11-28 09:40:27 -0800

    error fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,5 @@
-# Puppet Manifest: install_flask.pp
-# Description: Installs Flask version 2.1.0 using pip3 on Ubuntu 20.04 LTS
-
-# Install Python3 and pip3
-package { 'python3':
-  ensure => present,
-}
-
-package { 'python3-pip':
-  ensure => present,
-  require => Package['python3'],
-}
-
-# Install Flask version 2.1.0 using pip3
-package { 'Flask':
+#install flask on puppet link
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
-
