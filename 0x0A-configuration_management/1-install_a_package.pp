e4da9d5217e841cc7c52f008e0717d9931d13bab
simrett <leesim242@gmail.com>
2023-11-28 09:16:58 -0800

    install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #install flask on puppet link
+package { 'python3':
+  ensure => present,
+}
+
 package { 'python3-pip':
   ensure => present,
+  require => Package['python3'],
 }
 
 package { 'flask':
