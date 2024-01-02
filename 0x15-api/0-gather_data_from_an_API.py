91f9844e1107928313e4e09e7b78e9a33ab57e20
simrett <leesim242@gmail.com>
2024-01-02 13:17:24 -0800

    fixed errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""Script for parsing web data from an api
+"""script for parsing web data from an api
 """
-import requests
-import sys
-
-
 if __name__ == "__main__":
-    empId = sys.argv[1]
-    apiURL = "https://jsonplaceholder.typicode.com/users"
-    mainURL = apiURL + "/" + empId
+    import json
+    import requests
+    import sys
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    try:
+        employee_id = sys.argv[1]
+    except:
+        print('Usage: {} employee_id'.format(sys.argv[0]))
+        exit(1)
 
-    res = requests.get(mainURL)
-    empName = res.json().get('name')
+    # grab the info about the user
+    url = base_url + 'users?id={}'.format(employee_id)
+    response = requests.get(url)
+    user = json.loads(response.text)
+    name = user[0].get('name')
 
-    todoURL = mainURL + "/todos"
-    res = requests.get(todoURL)
-    todos = res.json()
-    done = 0
-    done_todos = []
+    # grab the info about the user's tasks
+    url = base_url + 'todos?userId={}'.format(employee_id)
+    response = requests.get(url)
+    objs = json.loads(response.text)
+    completed = 0
+    completed_tasks = []
+    for obj in objs:
+        if obj.get('completed'):
+            completed_tasks.append(obj)
+            completed += 1
 
-    for todo in todos:
-        if todo.get('completed'):
-            done_todos.append(todo)
-            doen += 1
-    print("Employee {} is done with tasks({}/{}):"
-            .format(empName, done, len(todos)))
-    for todo in done_todos:
-        print("\t {}".format(todo.get("title")))
+    # print the output about user's task completion
+    print("{} is done with tasks({}/{}):".format(name, completed, len(objs)))
+    # print the output title of completed tasks
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
