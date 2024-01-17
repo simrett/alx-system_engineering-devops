e4259ca62159882d291787991ec711b5f3bb2d5e
simrett <leesim242@gmail.com>
2024-01-17 11:23:49 -0800

    mod

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-""" Fetch the Reddit API number of subscribers """
+"""script for parsing web data from an api
+"""
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """ fetching the api """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """api call to reddit to get the number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/'
     headers = {
-        "User-Agent": "curl/7.81.0"}
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code == 404:
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    # grab info about all users
+    url = base_url + '{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    resp = json.loads(response.text)
+
+    try:
+        # grab the info about the users' tasks
+        data = resp.get('data')
+        subscribers = data.get('subscribers')
+    except:
         return 0
-    subs = res.json().get("data", {})
-    return subs.get("subscribers")
+    if subscribers is None:
+        return 0
+    return int(subscribers)
